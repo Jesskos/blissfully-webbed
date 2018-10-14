fb445a969d7d62eeb1afb8a0874e2d9668f9f9ce
Jessica Koscheka <jesskos@gmail.com>
2018-10-13 19:12:08 -0700

    established intial routes

--- server.py
+++ server.py
@@ -1,2 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+
 app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    pass
+
+@app.route('/details')
+def get_details():
+	pass
+
+app.route('/accommodations')
+def get_accomodations():
+	pass
+
+@app.route('/story')
+def get_story():
+	pass
+
+@app.route('/photos')
+def view_photos():
+	pass
+
+
+@app.route('/registry')
+def buy_buy_buy():
+	pass
+
+@app.route('/rsvp')
+def rsvp():
+	pass
+
+@app.route('/message_board')
+def view_message_board():
+	pass
+
+
+if __name__ == '__main__':
+	app.run()
+
