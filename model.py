a9559213ccade1a119859565ce3790311dffef12
Jessica Koscheka <jesskos@gmail.com>
2018-11-05 19:39:54 -0800

    added model table

--- model.py
+++ model.py
@@ -9,11 +9,19 @@ class User(db.Model):
 	user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
 	user_first_name = db.Column(db.String(40), nullable=False)
 	user_last_name = db.Column(db.String(40), nullable=False)
-	user_last_name = db.Column(db.String(50), nullable=False)
 	user_type = db.Column(db.Integer, nullable=True)
 
 
 
+class RSVP(db.Model):
+	__tablename__ = "rsvps"
+
+	rsvp_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	user_first_name = db.Column(db.String(40), nullable=False)
+	user_last_name = db.Column(db.String(40), nullable=False)
+	user_email = db.Column(db.String(40), nullable=True)
+	user_phone = db.Column(db.String(40), nullable=True)
+	attending? = db.Column(db.Boolean, nullable=False)
 
 
 
