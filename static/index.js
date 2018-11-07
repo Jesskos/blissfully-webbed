d3a754c01dcab722d317707a5e38e2c3f8fab19c
Jessica Koscheka <jesskos@gmail.com>
2018-11-06 19:42:35 -0800

    working in AJAX request to server with info from form

--- static/index.js
+++ static/index.js
@@ -2,6 +2,21 @@
 function submitRSVP(evt) {
 	evt.preventDefault();
 	console.log(evt)
+
+
+	formValues = $('form').serializeArray()
+	let guestInfo = {
+		"first": formValues[0]["value"],
+		"last": formValues[1]["value"],
+		"rsvp": formValues[2]["value"],
+		"phone": formValues[3]["value"],
+		"email": formValues[4]["value"]
+	}
+
+	let otherRsvps = $('#other-rsvps').find(":selected").text();
+
+	console.log(otherRsvps);
+	console.log(guestInfo)
 }
 
 
