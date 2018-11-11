e2b6d7fd1f11f777fdeb719b7d335ad4ffe0a771
Jessica Koscheka <jesskos@gmail.com>
2018-11-10 18:34:26 -0800

    made modal for other rsvps

--- static/index.js
+++ static/index.js
@@ -7,8 +7,8 @@ function rsvpRequestReceived(result) {
 function submitRSVP(evt) {
 	evt.preventDefault();
 	console.log(evt)
+	let formValues = $('form').serializeArray()
 	let otherRsvps = $('#other-rsvps').find(":selected").text();
-	formValues = $('form').serializeArray()
 	let guestInfo = {
 		"first": formValues[0]["value"],
 		"last": formValues[1]["value"],
@@ -17,15 +17,40 @@ function submitRSVP(evt) {
 		"email": formValues[4]["value"], 
 		"others": otherRsvps
 	}
+	let modal = document.getElementById('rsvp-modal')
 
 
-	console.log(guestInfo)
+	console.log(guestInfo['others'])
+	if (otherRsvps != "0") {
+		generateRsvp(otherRsvps)
+		modal.style.display="block"
+	} else { $.post("/rsvp_response",
+		     guestInfo, 
+		     rsvpRequestReceived);
+	}
 
-	$.post("/rsvp_response",
-		guestInfo, 
-		rsvpRequestReceived);
 }
 
-	
+function closeModal(evt) {
+	console.log("clicked x")
+	let modal = document.getElementById('rsvp-modal');
+	modal.style.display="none";
+}
 
-$(".rsvp-form").on("submit", submitRSVP)
+function generateRsvp(otherRsvps) {
+	let modalContent = document.getElementById('model-content');
+	let newForm = '<form action="/rsvp_response" class="rsvp-form" method="post">
+	First Name: <input type="text" name="fname" id="first-name" required> 
+  	Last Name: <input type="text" name="lname" id="last-name" required>
+    Attending? 
+    <input type="radio" name="rsvp" value="yes"> Yes
+    <input type="radio" name="rsvp" value="no"> No 
+     Phone number? <input type="text" name="phone" placeholder="optional"> 
+     What is your email address? <input type="text" name="email" placeholder="optional"> <br>
+     </form>'
+     modalContent.innerHTML = newForm
+}
+	
+$("#rsvp-button").on("click", submitRSVP)
+$(".close").on("click", closeModal)
+
