fc91dee53ef804ee734ea42be6dd6c8fa9efd108
Jessica Koscheka <jesskos@gmail.com>
2018-12-14 18:10:46 -0800

    modified form to check for number of RSVPs, then to remove section which asks for additional RSVPs when server gives a response

--- static/rsvp.js
+++ static/rsvp.js
@@ -7,23 +7,35 @@ function submitRSVP(evt) {
 	console.log('in function')
 	evt.preventDefault();
 	let rsvpFormValues = $('form').serializeArray();
+	console.log(rsvpFormValues)
 	let otherRsvps = $('#other-rsvps').find(":selected").text();
 	let guestInfo = {
 		"first": rsvpFormValues[0]["value"],
 		"last": rsvpFormValues[1]["value"],
-		"rsvp": rsvpFormValues[2]["value"],
+		"email": rsvpFormValues[2]["value"],
 		"phone": rsvpFormValues[3]["value"],
-		"email": rsvpFormValues[4]["value"], 
+		"rsvp": rsvpFormValues[4]["value"], 
+		"update": rsvpFormValues[5]["value"],
 		"others": otherRsvps
 	};
 	
 	console.log(guestInfo)
-	$.post('/rsvp_response', guestInfo, getConfirmation);
+	$.post('/rsvp_response', 
+		guestInfo,
+		getConfirmation);
 
 }
 
 function getConfirmation(result) {
-	alert('Done!')
+	console.log(result)
+	if (result["num_guests"] != "0") {
+		document.getElementById("submit-rsvp").reset();
+		$("#other-rsvps-space").remove()
+		$("#self-or-guest").html("<h1>Please Enter Guest Information</h1>")
+	} else { 
+		$("#rsvp-space").remove() 
+	};
+	alert(result["message"])
 }
 
 
