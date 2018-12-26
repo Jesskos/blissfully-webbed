238737c2f35abe2db523823b3a0f1120dc5e5117
Jessica Koscheka <jesskos@gmail.com>
2018-12-26 14:33:58 -0500

    default uri

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ def connect_to_db(app, uri='postgresql:///blissful_db'): # pragma: no cover
 	""" Connect the database to our Flask App"""
 
 	# Configure to use our PstgreSQL database
-	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")  
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",uri)  
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  			
 	db.app = app
 	db.init_app(app)
