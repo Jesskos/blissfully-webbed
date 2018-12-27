a121fe65dbecb5b43bb07003de2018b8bcc3f4a9
Jessica Koscheka <jesskos@gmail.com>
2018-12-27 12:07:14 -0500

    updated Profile for migrations

--- manage.py
+++ manage.py
@@ -1,13 +1,15 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from server import app, db
+from server import app, db, connect_to_db
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
+connect_to_db(app)
 manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
+
     manager.run()
